554c2783d663bdde7ea64f91cd165b57dc0ad0bd
Susmita C S <susmitacs1209@gmail.com>
2026-01-18 19:15:49 +0530

    Update lab1_pr3.java

--- lab1_pr3.java
+++ lab1_pr3.java
@@ -1,37 +1,36 @@
 import java.util.Scanner;
     
 interface Operation{    //interface contains method declarations
-    void sum();
-    void avg();
+    void sum();         //these are implicitly public abstract 
+    void avg();         //an interface cannot have interface variables except 'public static final' (that are used in methods ig)
     void prod();
     void largest();
     void smallest();
 }
 abstract class Numbers implements Operation{    //we can't create objects of this class
     int a,b,c;                                  //parent class to other classes
-    Numbers(int a, int b, int c)                //methods don't need to be implemented here hence it is flexible
+    Numbers(int a, int b, int c)                //methods don't need to be implemented here hence it is flexible (here i have implemented)
     {                                           //ex: diff child classes can have diff ways to implement abstract method
         this.a = a;
         this.b = b;
         this.c = c;
     }
-}
-class NumbOper extends Numbers{
-    NumbOper(int a, int b, int c){
-        super(a, b, c);                         //creates constructor to get values a,b,c from Numbers class (still dk why its not there automatically as its child and inherits::NOT TAUGHT YET)
-    }
-    public void sum()
+    @Override
+    public void sum()           //it is safe to add @override notation here as it is replacing the declaration of interface here (usually used in inheritance)
     {
         System.out.println("sum is: "+(a+b+c));
     }
+    @Override
     public void avg()
     {
-        System.out.println("avg is: "+(a+b+c)/3);
+        System.out.println("avg is: "+(a+b+c)/3.0);
     }
+    @Override
     public void prod()
     {
         System.out.println("prod is: "+(a*b*c));
     }
+    @Override
     public void largest()
     {
         if(a>b && a>c)
@@ -41,6 +40,7 @@ class NumbOper extends Numbers{
         else
             System.out.println("c is greatest");                 
     }
+    @Override
     public void smallest()
     {
         if(a<b && a<c)
@@ -50,23 +50,37 @@ class NumbOper extends Numbers{
         else
             System.out.println("c is smallest");
     }
+    @SuppressWarnings("unused")     //this warning is generated as i didnt use this function within this class
+    abstract void randomFunction();     //in different classes, it can have diff bodies (sim to an interface function)
+}
+class NumbOper extends Numbers{
+    NumbOper(int a, int b, int c){
+        super(a, b, c);               //creates constructor to get values a,b,c from Numbers class (still dk why its not there automatically as its child and inherits)
+    }                                 //super is needed as constructors are not inherited so they need to be called
+    @Override
+    void randomFunction()
+    {
+        System.out.print("this program has an interface for method initialisations, ");
+        System.out.print("an abstract class as a blueprint for further classes, ");
+        System.out.print("and this class which inherits all the properties from the abstract class ");
+    }
 }
 public class lab1_pr3 {
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);    //initialising scanner
-        System.out.println("enter a: ");
+        System.out.print("enter a: ");
         int n1=sc.nextInt();
-        System.out.println("enter b: ");
+        System.out.print("enter b: ");
         int n2=sc.nextInt();
-        System.out.println("enter c: ");
+        System.out.print("enter c: ");
         int n3=sc.nextInt();  
         NumbOper object = new NumbOper(n1,n2,n3);  
         object.sum();
         object.avg();
         object.prod();
         object.largest();
-        object.smallest();  
+        object.smallest(); 
+        object.randomFunction(); 
         sc.close();  
     }
 }
-
